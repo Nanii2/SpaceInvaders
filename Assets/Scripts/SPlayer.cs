85d3b6e9511920333e725fae60232efc14c9cedf
Nanii2 <wandaoliver15@gmail.com>
2024-02-14 16:55:29 +0100

    0.1

---
+++ Assets/Scripts/SPlayer.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPlayer : MonoBehaviour
+{
+    [Tooltip("Prefab de la bala")]
+    public GameObject prefabBullet; //prefab de la bala
+    [Tooltip("Velocidad del jugador en unidades de unity / segundo")]
+    public float speed = 2; // velocidad del jugador
+
+    //teclas para input configurable
+    public KeyCode shootKey = KeyCode.Space;
+    public KeyCode moveLeftKey = KeyCode.A;
+    public KeyCode moveRightKey = KeyCode.D;
+
+    public Transform posDisparo;
+
+    public bool canShoot = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canShoot && Input.GetKeyDown(shootKey))
+        {
+            //dispara
+            Shoot();
+        }
+        else if(Input.GetKey(moveLeftKey))
+        {
+            //voy a la izquierda
+            transform.position += new Vector3(speed, 0, 0) * Time.deltaTime;
+
+        }
+        else if (Input.GetKey(moveRightKey))
+        {
+            // voy a la derecha
+            transform.position += new Vector3(-speed, 0, 0) * Time.deltaTime;
+
+        }
+    }
+
+    private void Shoot()
+    {
+
+        Debug.Log("Disparo");
+        Instantiate(prefabBullet, transform.position, Quaternion.identity);
+        canShoot = false;
+    }
+}
---
+++ Assets/Scripts/SPlayerBullet.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SPlayerBullet : MonoBehaviour
+{
+    public float speed = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(0, speed, 0) * Time.deltaTime;
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "SBorder")
+        {
+            Destroy(gameObject);
+
+        }
+    }
+
+    private void OnDestroy()
+    {
+        Debug.Log("Bala Destruida");
+
+    }
+}
